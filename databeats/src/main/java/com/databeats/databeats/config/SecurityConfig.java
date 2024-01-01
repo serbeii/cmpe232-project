7a169ac28e915fbe0005f0f86bb162e3564c18ed
serbeii <cem.serimer@tedu.edu.tr>
2024-01-01 22:24:37 +0300

    fix security

--- databeats/src/main/java/com/databeats/databeats/config/SecurityConfig.java
+++ databeats/src/main/java/com/databeats/databeats/config/SecurityConfig.java
@@ -2,14 +2,19 @@ package com.databeats.databeats.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
 
 import com.databeats.databeats.model.User;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -22,5 +27,15 @@ public class SecurityConfig {
         UserDetails admin = new User("admin", passwordEncoder().encode("adminpass"), "ADMIN"); 
         return new InMemoryUserDetailsManager(user1, admin);
     }
-
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authorize) -> authorize
+				.anyRequest().authenticated()
+			)
+			.httpBasic(Customizer.withDefaults())
+			.formLogin(Customizer.withDefaults());
+        return http.build();
+    }
 }
