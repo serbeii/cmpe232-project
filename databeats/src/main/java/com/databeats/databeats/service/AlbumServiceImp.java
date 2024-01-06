05a9a0313362ec4938012b35b8185ea565d8f1e6
Cem Serimer <118691707+serbeii@users.noreply.github.com>
2024-01-06 18:32:44 +0300

    Update AlbumServiceImp.java

--- databeats/src/main/java/com/databeats/databeats/service/AlbumServiceImp.java
+++ databeats/src/main/java/com/databeats/databeats/service/AlbumServiceImp.java
@@ -7,18 +7,23 @@ import org.springframework.stereotype.Service;
 
 import com.databeats.databeats.model.Album;
 import com.databeats.databeats.repository.AlbumRepository;
+import com.databeats.databeats.dto.AlbumDTO;
+import com.databeats.databeats.model.Artist;
+import com.databeats.databeats.repository.ArtistRepository;
 
 import jakarta.transaction.Transactional;
 
 @Service
 public class AlbumServiceImp implements AlbumService {
-
-    private  AlbumRepository albumRepository;
+    
+    @Autowired
+    AlbumRepository albumRepository;
+   
+    @Autowired 
+    ArtistRepository artistRepository;
 
     @Autowired
-    public AlbumServiceImp(AlbumRepository albumRepository) {
-        this.albumRepository = albumRepository;
-    }
+    ArtistService artistService;
 
     @Override
     @Transactional
@@ -38,6 +43,22 @@ public class AlbumServiceImp implements AlbumService {
     public Long deleteAlbum(long album_id) {
         albumRepository.deleteById(album_id);
         return album_id;
+    }
+
+    @Override
+    @Transactional
+    public void saveAlbum(AlbumDTO albumDTO) {
+        String artistName = albumDTO.getArtistName();
+        String title = albumDTO.getTitle();
+        LocalDate releaseDate = albumDTO.getReleaseDate();
+        String genre = albumDTO.getGenre();
+        Artist artist = artistService.getArtistByName(artistName);
+
+        if (artist == null) {
+            artistRepository.addArtist(artistName);
+            artist = artistService.getArtistByName(artistName);
+        }   
+        albumRepository.saveAlbum(title, releaseDate, genre, artist.getArtistId());
     }
 }
     
